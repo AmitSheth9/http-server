5fbe8008aabf6c37dcfad7d8d5cd62370fea710e
AmitSheth9 <amit.sheth9@gmail.com>
2021-11-02 10:18:57 -0700

    wrote test and app.js, test failing

---
+++ __tests__/app.test.js
@@ -0,0 +1,26 @@
+const request = require('supertest');
+const { rm, mkdir } = require('fs/promises');
+const app = require('../lib/app');
+// eslint-disable-next-line no-unused-vars
+const SimpleDb = require('../lib/simple-db');
+
+const rootDir = `${__dirname}/store`;
+
+describe('cat CRUD API', () => {
+  beforeEach(() => {
+    return rm(rootDir, { force: true, recursive: true }).then(() =>
+      mkdir(rootDir, { recursive: true })
+    );
+  });
+  afterAll(() => {
+    return rm(rootDir, { force: true, recursive: true }).then(() =>
+      mkdir(rootDir, { recursive: true })
+    );
+  });
+  it('creates a new cat and returns it via POST', async () => {
+    const cat = { name: 'felix', age: 9 };
+    const res = await request(app).post('/cats').send(cat);
+
+    expect(res.body).toEqual(JSON.stringify(cat));
+  });
+});
