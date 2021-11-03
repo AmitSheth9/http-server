2c1d5ad917a3c345083dba345f43a63e68e41eaa
AmitSheth9 <amit.sheth9@gmail.com>
2021-11-03 08:38:05 -0700

    added delete and push functions with test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -6,31 +6,70 @@ const SimpleDb = require('../lib/simple-db');
 
 const rootDir = './lib/store';
 
+
+
 describe('cat CRUD API', () => {
-  /*beforeEach(() => {
+  beforeEach(() => {
+    return rm(rootDir, { force: true, recursive: true }).then(() =>
+      mkdir(rootDir, { recursive: true })
+    );
+  });
+  /*
+  afterAll(() => {
     return rm(rootDir, { force: true, recursive: true }).then(() =>
       mkdir(rootDir, { recursive: true })
     );
   });
   */
-  /*afterAll(() => {
-    return rm(rootDir, { force: true, recursive: true }).then(() =>
-      mkdir(rootDir, { recursive: true })
-    );
-  });*/
-  it.skip('creates a new cat and returns it via POST', async () => {
+  it('creates a new cat and returns it via POST', async () => {
     const cat = { name: 'felix', age: 9 };
-    //const db = new SimpleDb()
+    const db = new SimpleDb(rootDir);
+    await db.save(cat);
     const res = await request(app).post('/cats').send(cat);
 
     expect(res.body).toEqual({ ...cat, id: expect.any(String) });
   });
-  it('gets a dog by id', async () => {
-    const cats = [{ name: 'felix', age: 9, id: expect.any(String) }, { name: 'felix', age: 9, id: expect.any(String) }, { name: 'felix', age: 9, id: expect.any(String) }];
+  it('gets all cats', async () => {
+    const cats = [{ name: 'felix', age: 9, id: expect.any(String) }, { name: 'sahil', age: 6, id: expect.any(String) }, { name: 'sam', age: 3, id: expect.any(String) }];
+    const db = new SimpleDb(rootDir);
+    await db.save(cats[0]);
+    await db.save(cats[1]);
+    await db.save(cats[2]);
     const res = await request(app).get('/cats');
 
-    expect(res.body).toEqual(cats);
+    expect(res.body).toEqual(expect.arrayContaining(cats));
   });
 
-  it('get')
+  it('gets cat by id', async () => {
+    const cat = { name: 'felix', age: 7, color: 'black' };
+    const db = new SimpleDb(rootDir);
+    await db.save(cat);
+
+    const res = await request(app).get(`/cats/${cat.id}`);
+
+    expect(res.body).toEqual(cat);
+  });
+
+  it('deletes cat by id', async () => {
+    const cat = { name: 'felix', age: 7, color: 'black' };
+    const db = new SimpleDb(rootDir);
+    await db.save(cat);
+
+    await request(app).delete(`/cats/${cat.id}`);
+    console.log(await db.get(cat.id));
+    expect(await db.get(cat.id)).toEqual(null);
+
+  });
+
+  it('updates a cat by id using put', async () => {
+    const cat = { name: 'felix', age: 6, color: 'yellow' };
+    const db = new SimpleDb(rootDir);
+    await db.save(cat);
+    console.log(await db.getAll());
+    
+    const res = await request(app).put(`/cats/${cat.id}`).send({ name: 'charles', age: 6, color: 'yellow', id: cat.id });
+    console.log(await db.getAll());
+    expect(res.body).toEqual(await db.get(cat.id));
+  });
+ 
 });
